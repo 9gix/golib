1524fd857a31c4902564ca305c82f5d056b31ae1
Eugene <yeo.eugene.oey@gmail.com>
2012-10-20 14:19:36 +0800

    Added Logo

--- booku/urls.py
+++ booku/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 admin.autodiscover()
 
@@ -8,3 +12,6 @@ urlpatterns = patterns('',
     url(r'^book/', include('catalog.urls', namespace='catalog')),
     url(r'^$', 'booku.views.index'),
 )
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
