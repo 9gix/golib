abcf032539fe92e4da9b158cc4a2c7d7a9ad69b8
Eugene <yeo.eugene.oey@gmail.com>
2012-10-23 02:34:59 +0800

    Small Refactoring Code

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import *
 from accounts.forms import UserRegistrationForm, UserProfileForm
-from registration.views import register
 
 urlpatterns = patterns('',
-        url(r'^register/$', register,
+        url(r'^register/$', 'registration.views.register',
             {'backend':'registration.backends.default.DefaultBackend',
-                'form_class':UserRegistrationForm}),
+            'form_class':UserRegistrationForm}),
+        url(r'^edit/', 'profiles.views.edit_profile',
+            {'form_class':UserProfileForm}),
         url(r'', include('registration.backends.default.urls')),
-        url(r'^edit/', 'profiles.views.edit_profile',{'form_class':UserProfileForm}),
         url(r'', include('profiles.urls')),
 )
