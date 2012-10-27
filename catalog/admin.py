0a832693b0cf957ec901ff3aecf2472b356832e2
Eugene <yeo.eugene.oey@gmail.com>
2012-10-27 12:15:47 +0800

    Admin List Display

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,6 +5,8 @@ from catalog.forms import BookModelForm
 class BookAdmin(admin.ModelAdmin):
     form = BookModelForm
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ('isbn','title',)
+    list_display_links = ('isbn','title',)
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookOwner)
