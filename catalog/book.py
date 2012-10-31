651e989dc8971eac2b244b563e136362abd146a6
Eugene <yeo.eugene.oey@gmail.com>
2012-11-01 01:33:24 +0800

    Fix Slug Error and check mapping of ISBN

--- catalog/book.py
+++ catalog/book.py
@@ -1,41 +1,46 @@
 from gdata.books.service import BookService
 from catalog.models import Book
 
-def fetch_book_detail(isbn):
+def fetch_book_detail_from_google(isbn):
     """Fetch book from Google Book Service and Return dict of book info"""
     service = BookService()
     result = service.search(isbn)
-    try:
-        book = result.entry[0]
-    except IndexError:
-        return {}
-    book_dict = book.to_dict()
+    book_dict = {}
+    matched_book = None
+    for book in result.entry:
+        for identifier in book.identifier:
+            if isbn in identifier.text:
+                matched_book = book
+                break
+
+
+    book_dict = matched_book.to_dict()
+
+    if matched_book:
+        for identifier in matched_book.identifier:
+            """
+            Book Identifiers:
+                [('google_id', 'dwSfGQAACAAJ'),
+                ('ISBN', '0132350882'),
+                ('ISBN', '9780132350884')]
+            """
+            if 'ISBN' in identifier.text:
+                isbn = identifier.text.split(':',1)[1]
+                if len(isbn) == 10:
+                    book_dict['isbn10'] = isbn
+                elif len(isbn) == 13:
+                    book_dict['isbn13'] = isbn
     return book_dict
 
 def search_and_save_to_db(isbn):
     """Search and save Book details into database and return the book object"""
-    book_data = fetch_book_detail(isbn)
+    book_data = fetch_book_detail_from_google(isbn)
 
     if book_data:
         title = book_data.get('title')
         description = book_data.get('description','')
         identifier_list = book_data.get('identifiers', [])
-        isbn10, isbn13 = None, None
-        for identifier in identifier_list:
-            """Sample Identifiers:
-                [('google_id', 'dwSfGQAACAAJ'),
-                ('ISBN', '0132350882'),
-                ('ISBN', '9780132350884')]"""
-            try:
-                if identifier[0] == 'ISBN':
-                    if len(identifier[1]) == 10:
-                        isbn10 = identifier[1]
-                    elif len(identifier[1]) == 13:
-                        isbn13 = identifier[1]
-            except IndexError:
-                # TODO: log this error, perhaps google change its structure
-                pass
-
+        isbn10, isbn13 = book_data.get('isbn10', None), book_data.get('isbn13',None)
         try:
             # ISBN that we sent should matched with the result from Google
             if len(isbn) == 10:
